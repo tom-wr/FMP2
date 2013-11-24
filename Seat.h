1cf5191495fd73dc1dd03b749f0b9089311a110f
tom-wr <tomwriggles@hotmail.com>
2013-11-24 14:18:16 +0000

    initial commit

---
+++ Seat.h
@@ -0,0 +1,20 @@
+/*
+ * Seat.h
+ *
+ *  Created on: 24 Nov 2013
+ *      Author: tomto
+ */
+
+#ifndef SEAT_H_
+#define SEAT_H_
+
+namespace Seat{
+
+
+	enum Type
+	{
+		FIRST,
+		ECONOMY
+	};
+}
+#endif /* SEAT_H_ */
